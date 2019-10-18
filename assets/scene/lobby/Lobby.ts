f04208b989322fe6f85e5c06d41572ca449ac190
dxp <dongxiaoping>
2019-10-18 22:11:34 +0800

    优化大厅页面元素尺寸

--- assets/scene/lobby/Lobby.ts
+++ assets/scene/lobby/Lobby.ts
@@ -8,23 +8,24 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Lobby extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
+    @property(cc.Prefab)
+    label: cc.Prefab = null;
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
 
-    start () {
+    start() {
         console.log('应用启动')
+        //  var anyPrefab = cc.instantiate(this.anyPrefab);
+        //anyPrefab.getComponent(cc.Label).string = '文字';
+       // anyPrefab.setPosition(cc.p(0, 0));
+       // this.node.addChild(anyPrefab);
     }
 
     // update (dt) {}
