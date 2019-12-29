3e965234f1070757c7a41c9635253d6ca51d2b35
dxp <dongxiaoping>
2019-12-29 10:31:12 +0800

    修复若干bug

--- assets/mock/UserInfo.ts
+++ assets/mock/UserInfo.ts
@@ -1,7 +1,7 @@
 import {UserInfo, userType} from '../store/User/UserBase'
 export const userInfo: UserInfo  = {
     id: '6666660',
-    nick: '苹果',
+    nick: '我爱吃苹果',
     icon: 'renwu/1_10',
     type: userType.NORMAL_USER,
     score: 23,
