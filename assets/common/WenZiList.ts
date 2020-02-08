4a2f4195a464dd37065246a26b55cb20974ef0d4
dxp <dongxiaoping>
2020-02-08 16:45:33 +0800

    优化表情列表

--- assets/common/WenZiList.ts
+++ assets/common/WenZiList.ts
@@ -1,4 +1,10 @@
 export const wenZiList: any = [
     { content: '搞起搞起我们单挑！', voice: 'a' },
-    { content: '大家一起嗨起！', voice: 'a' }
+    { content: '大家一起嗨起！', voice: 'a' },
+    { content: '测试1', voice: 'a' },
+    { content: '测试2', voice: 'a' },
+    { content: '测试3', voice: 'a' },
+    { content: '测试4', voice: 'a' },
+    { content: '测试5', voice: 'a' },
+    { content: '测试6', voice: 'a' }
 ]
