9d5e0eb9599a71addbaca024a843863e9374f44b
dxp <dongxiaoping>
2020-04-29 13:05:21 +0800

    优化

--- assets/component/SharePanel/SharePanel.ts
+++ assets/component/SharePanel/SharePanel.ts
@@ -1,6 +1,7 @@
 import RoomManage from "../../store/Room/RoomManage";
 import ConfigManage from "../../store/Config/ConfigManage";
 import UserManage from "../../store/User/UserManage";
+import { webCookie } from "../../common/Util";
 
 const {ccclass, property} = cc._decorator;
 
@@ -23,8 +24,9 @@ export default class NewClass extends cc.Component {
             let gameUrl = ConfigManage.getGameUrl()
             let contentInfo = UserManage.userInfo.nick+"邀请您一起玩，代开房间【"+roomNum+"】,人数上限"+memberLimit+
             "，局数"+playCount+"，最高下"+costLimit+
-            ",点击URL地址进入："+gameUrl+"?roomId="+roomNum
-            this.webViewPart.url = "https://www.toplaygame.cn/login/copyPage?copy="+contentInfo
+            ",点击URL地址进入游戏："+gameUrl+"?roomId="+roomNum
+            webCookie.setItem("share", contentInfo, null)
+           this.webViewPart.url = "https://www.toplaygame.cn/login/copyPage"
         }catch(e){}
 
     }
