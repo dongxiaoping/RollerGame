e7e09a760741d35c801482fd996a7b5a322cba2d
Administrator <466476120@qq.com>
2021-02-07 21:53:45 +0800

    test

--- assets/store/Config/ConfigManage.ts
+++ assets/store/Config/ConfigManage.ts
@@ -12,7 +12,7 @@ class ConfigManage {
     private audioUrl: string = '' //音频文件地址
     private rapLandlordTime: number
     private gameUrl: string = '' //游戏地址
-    private informMessage: string = "滚筒子诚招代理，代理请于客服联系！文明游戏，禁止赌博，一旦发现利用平台从事赌博活动一律封号处理！"
+    private informMessage: string = "融和电科"
     private customerWechat: string = 'xxxx'
     private cheat: boolean = false //是否作弊
     private rollDiceTime: number = 4
